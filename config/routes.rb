967f540fa373ab86f93f18a74ce1a52bb661204c
bryn9696 <bryn2009@live.co.uk>
2022-08-02 12:04:21 +0100

    basic sign up, sign in working with user MVC, need to add log out button

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get 'home/index'
+  root 'home#index'
+  get "signup", to: "users#new"
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
+  resources :users, except: [:new]
 end
